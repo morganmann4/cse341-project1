c9cd888c3b869045511b18aa801ff46e9fdce011
Morgan Mann <momomann4@gmail.com>
2023-09-21 09:53:52 -0600

    installed swagger

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
+    //#swagger.tags=['Users']
     const userid = new ObjectId(req.params.id)
     const result = await mongodb.getDatabase().db().collection('users').find();
     result.toArray().then((users) => {
@@ -11,7 +12,8 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
-    const userid = new ObjectId(req.params.id)
+    //#swagger.tags=['Users']
+    const userId = new ObjectId(req.params.id)
     const result = await mongodb.getDatabase().db().collection('users').find({_id: userId});
     result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
@@ -19,7 +21,56 @@ const getSingle = async (req, res) => {
     });
 };
 
+const createUser = async (req, res) => {
+    //#swagger.tags=['Users']
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
+    if (response.acknowledged > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occured while creating the user')
+    }
+};
+
+const updateUser = async (req, res) => {
+    //#swagger.tags=['Users']
+    const userId = new ObjectId(req.params.id)
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().db().collection('users').replaceOne({ _id: userId }, user);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occured while updating the user')
+    }
+};
+
+const deleteUser = async (req, res) => {
+    //#swagger.tags=['Users']
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('users').deleteOne({ _id: userId });
+    if (response.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occured while deleteing the user')
+    }
+};
+
 module.exports = {
     getAll,
-    getSingle
+    getSingle,
+    createUser,
+    updateUser,
+    deleteUser
 };
--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 // Express web server example
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser')
 const mongodb = require('./data/database');
+const app = express();
 
- 
+const port = process.env.PORT || 3000; 
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+})
 //links to the routes folder
 app.use('/', require('./routes'));
-
-const port = process.env.PORT || 3000;
-
 
 mongodb.initDb((err) => {
     if(err){
@@ -17,7 +27,7 @@ mongodb.initDb((err) => {
     else{
         app.listen(process.env.PORT || port, () => {
             console.log('Web Server is listening at port ' + (process.env.PORT || port));
-          });
+        });
     }
 })
 
